e39a9d2900668c6a543cd8c177f4a461c9836e3e
AswathiMohan23 <achu999here@gmail.com>
2022-11-13 07:37:21 +0530

    [REFACTORED] : the cosw to print 2d array

--- src/com/java/DeckOfCardsPlayerMethods.java
+++ src/com/java/DeckOfCardsPlayerMethods.java
@@ -1,48 +1,35 @@
 package com.java;
 
-import java.util.LinkedList;
+import static com.java.PlayerDetails.randomRankCheck;
+import static com.java.PlayerDetails.randomSuitCheck;
 
-public class DeckOfCardsPlayerMethods {
-
-    static int[] queueArray=new int[9];
-    static PlayerDetails playerDetails=new PlayerDetails();
-    static LinkedList <String> firstPlayer=new LinkedList<>();
-    static LinkedList <String> secondPlayer=new LinkedList<>();
-    static LinkedList <String> thirdPlayer=new LinkedList<>();
-    static LinkedList <String> fourthPlayer=new LinkedList<>();
-    static LinkedList <String> rank1 =new LinkedList<>();
-    static LinkedList <String> rank2 =new LinkedList<>();
-    static LinkedList <String> rank3 =new LinkedList<>();
-    static LinkedList <String> rank4 =new LinkedList<>();
-
-
+public class DeckOfCardsPlayerMethods{
+    static String[][] firstPlayer = new String[3][3];
+    static String[][] secondPlayer = new String[3][3];
+    static String[][] thirdPlayer = new String[3][3];
+    static String[][] fourthPlayer = new String[3][3];
 
     public static void main(String[] args) {
-        addCards(firstPlayer,"player1",rank1);
-        addCards(secondPlayer,"player2",rank2);
-        addCards(thirdPlayer,"player3",rank3);
-        addCards(fourthPlayer,"player4",rank4);
+        cardDistribution(firstPlayer,"player1");
+        cardDistribution(secondPlayer,"player2");
+        cardDistribution(thirdPlayer,"player3");
+        cardDistribution(fourthPlayer,"player4");
     }
 
-    public static void addCards(LinkedList player, String playerName, LinkedList rank) {
-        for (int i =   0; i < 9; i++) {
-            String randomSuit = playerDetails.randomSuitCheck();
-            String randomRank = playerDetails.randomRankCheck();
-            player.add(randomSuit);
-            rank.add(randomRank);
+
+    public static void cardDistribution(String[][] player,String playerName) {
+        for(int i=0;i<3;i++){ // each player needs 9 cards 3 rows 3 columns =9
+            String rank=randomRankCheck();
+            for(int j=0;j<3;j++){
+                String suit=randomSuitCheck();
+                String input=suit+"-"+rank;
+                player[i][j]=input;
+            }
         }
-        System.out.println("\n\n======================="+playerName+"'s cards ========================\n"+"\n"+player+"\nrank : "+rank);
+        printingPlayerData(player,playerName);
     }
 
-
-    }
-
-
-
-
-
-
-  /*  private static void printingPlayerData(LinkedList<String> player, String playerName) {
+    private static void printingPlayerData(String[][] player,String playerName) {
         System.out.println("\n============================  "+playerName+"'s cards  ===============================");
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
@@ -52,6 +39,5 @@ public class DeckOfCardsPlayerMethods {
                 System.out.println(player[i][j]);
             }
         }
-    }*/
-
-
+    }
+}
